bc9823216b8cea00221e759f1575ea2699fecd24
Tolga Paksoy <tolga@paramander.com>
2022-09-28 22:24:23 +0200

    fix(core): not being able to inject context

--- packages/core/src/core.module-builder.ts
+++ packages/core/src/core.module-builder.ts
@@ -5,7 +5,7 @@ export const {
   ASYNC_OPTIONS_TYPE,
   OPTIONS_TYPE,
   ConfigurableModuleClass,
-  MODULE_OPTIONS_TOKEN,
+  MODULE_OPTIONS_TOKEN: SHOPIFY_CORE_OPTIONS,
 } = new ConfigurableModuleBuilder<ConfigInterface, 'forRoot'>()
   .setClassMethodName('forRoot')
   .setExtras({}, (definition) => ({
