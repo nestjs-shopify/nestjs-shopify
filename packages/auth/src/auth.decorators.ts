dfe9d464c11fb42f191f6bb50b8763bb46e42d50
Tolga Paksoy <tolga@paramander.com>
2022-09-29 00:22:02 +0200

    refactor(auth): resolve session from req param

--- packages/auth/src/auth.decorators.ts
+++ packages/auth/src/auth.decorators.ts
@@ -6,12 +6,12 @@ import {
   UseFilters,
   UseGuards,
 } from '@nestjs/common';
-import Shopify, { SessionInterface } from '@shopify/shopify-api';
-import type { IncomingMessage, ServerResponse } from 'http';
+import { SessionInterface } from '@shopify/shopify-api';
+import type { IncomingMessage } from 'http';
 import { AUTH_MODE_KEY } from './auth.constants';
 import { ShopifyAuthExceptionFilter } from './auth.filter';
 import { ShopifyAuthGuard } from './auth.guard';
-import { AccessMode } from './auth.interfaces';
+import { AccessMode, ShopifySessionRequest } from './auth.interfaces';
 
 export const UseShopifyAuth = (mode = AccessMode.Online) =>
   applyDecorators(
@@ -23,10 +23,11 @@ export const UseShopifyAuth = (mode = AccessMode.Online) =>
 export const CurrentSession = createParamDecorator<
   unknown,
   ExecutionContext,
-  Promise<SessionInterface | undefined>
+  SessionInterface | undefined
 >((_data: unknown, ctx: ExecutionContext) => {
-  const req = ctx.switchToHttp().getRequest<IncomingMessage>();
-  const res = ctx.switchToHttp().getResponse<ServerResponse>();
+  const req = ctx
+    .switchToHttp()
+    .getRequest<ShopifySessionRequest<IncomingMessage>>();
 
-  return Shopify.Utils.loadCurrentSession(req, res);
+  return req.shopifySession;
 });
